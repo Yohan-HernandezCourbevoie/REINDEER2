de4e092bbc55ca44cbb68256d2123487b62198c9
Lucas Robidou <lucas.robidou@uni-bielefeld.de>
2026-03-18 10:43:45 +0100

    split merge module

---
+++ src/reindeer2/merge/merge_partitions.rs
@@ -0,0 +1,219 @@
+use itertools::Itertools;
+use roaring::RoaringBitmap;
+use std::{
+    cmp::min,
+    fs::File,
+    io::{self, BufWriter, Write},
+    path::Path,
+    time::Instant,
+};
+
+use crate::reindeer2::{
+    create_and_reserve_tar_get_file, filter::load_raw_bloom_filter,
+    merge::merge_partition_slices_interleaved, NB_FILE_IN_AN_INDEX,
+};
+
+/// Merges all partitions of an index being built.
+///
+/// The initial partitions
+pub fn merge_all_partitions_of_chunks(
+    chunk_files_dir: &str,
+    output_dir: &str,
+    partitioned_bf_size: usize,
+    abundance_number: usize,
+    color_counts_per_chunk: Vec<usize>, // number of colors in each chunk
+    num_partitions: usize,
+) -> io::Result<()> {
+    let start_time = Instant::now();
+
+    let nb_chunk = color_counts_per_chunk.len();
+    let nb_partition_in_a_file = num_partitions.div_ceil(NB_FILE_IN_AN_INDEX);
+
+    (0..NB_FILE_IN_AN_INDEX)
+        .into_iter()
+        .try_for_each(|file_id| {
+            let range_start = nb_partition_in_a_file * file_id;
+            let range_end = min(nb_partition_in_a_file * (file_id + 1), num_partitions);
+            let mut range = range_start..range_end;
+
+            let path =
+                Path::new(output_dir).join(format!("partition_bloom_filters_group{file_id}.bin"));
+            let len: usize = range
+                .try_len()
+                .expect("range object should have a known length");
+            let len = u64::try_from(len)
+                .expect("should have less than u64::MAX partitions in a single file");
+            let mut file = create_and_reserve_tar_get_file(&path, len);
+
+            range.try_for_each(|partition_idx| {
+                // collect chunk files for the current partition
+                let chunk_files_for_partition: Vec<String> = (0..nb_chunk)
+                    .map(|chunk_idx| {
+                        format!(
+                            "{}/partition_bloom_filters_c{}_p{}.bin",
+                            chunk_files_dir, chunk_idx, partition_idx
+                        )
+                    })
+                    .collect();
+
+                // merge all chunks for the current partition + serialize
+                merge_partition_bloom_filters(
+                    &chunk_files_for_partition,
+                    partitioned_bf_size,
+                    abundance_number,
+                    &color_counts_per_chunk,
+                    &mut file,
+                )?;
+
+                Ok::<(), io::Error>(())
+            })
+        })?;
+
+    let elapsed_time = start_time.elapsed();
+    log::info!(
+        "All partitions merged and written to disk in {:.2?}",
+        elapsed_time
+    );
+
+    Ok(())
+}
+
+/// Merges all filter from files chunk_files into a unique tar_get file.
+fn merge_partition_bloom_filters(
+    chunk_files: &[String],
+    partitioned_bf_size: usize,
+    abundance_number: usize,
+    color_counts: &[usize], // number of colors for each chunk
+    output_file: &mut File,
+) -> io::Result<RoaringBitmap> {
+    if chunk_files.is_empty() || chunk_files.len() != color_counts.len() {
+        return Err(io::Error::new(
+            io::ErrorKind::InvalidInput,
+            "Mismatch between chunk files and color counts, or no chunks provided.",
+        ));
+    }
+
+    // OPTIMIZE we may be able to load only one filter at a time
+    let filters: Vec<(RoaringBitmap, usize)> = chunk_files
+        .iter()
+        .map(|chunk_file| match load_raw_bloom_filter(chunk_file) {
+            Ok(filter) => filter,
+            Err(e) => {
+                log::error!("Failed to load Bloom filter {}: {}", chunk_file, e);
+                panic!("Failed to load Bloom filter {}: {}", chunk_file, e);
+            }
+        })
+        .collect();
+
+    // write all slices in the right order in a  larger filter
+    let final_bf = merge_partition_slices_interleaved(
+        &filters,
+        partitioned_bf_size,
+        abundance_number,
+        // color_counts,
+    );
+
+    // write as a targ_get file
+    let serializer =
+        |writer: &mut BufWriter<&mut File>, bitmap: &RoaringBitmap| bitmap.serialize_into(writer);
+    // let mut output_file = output_file.lock().unwrap();
+    tar_get::append_element(output_file, &final_bf, serializer).map_err(|err| {
+        io::Error::other(format!("Error while eppending to a tar_get file: {err}"))
+    })?;
+    output_file.flush()?;
+    Ok(final_bf)
+}
+
+#[cfg(test)]
+mod tests {
+
+    use std::fs::OpenOptions;
+
+    use super::*;
+
+    use crate::reindeer2::test_utils::AutoRemoveDirectory;
+    use rstest::{fixture, rstest};
+
+    #[fixture]
+    pub fn random_directory() -> AutoRemoveDirectory {
+        AutoRemoveDirectory::create_random()
+    }
+
+    #[rstest]
+    fn test_merge_partition_bloom_filters(random_directory: AutoRemoveDirectory) {
+        use roaring::RoaringBitmap;
+        use std::fs::{create_dir_all, File};
+        use std::io::Write;
+
+        let test_dir = random_directory.filename().to_str().unwrap();
+        create_dir_all(test_dir).expect("Failed to create test directory");
+
+        let chunk1_path = format!("{}/chunk1_p0.bin", test_dir);
+        let chunk2_path = format!("{}/chunk2_p0.bin", test_dir);
+        let chunk3_path = format!("{}/chunk3_p0.bin", test_dir);
+
+        let partitioned_bf_size = 2;
+        let abundance_number = 3;
+        // test Bloom filters
+        let mut bf1 = RoaringBitmap::new();
+        bf1.insert(1);
+        bf1.insert(2); //011000 000000
+        let mut bf2 = RoaringBitmap::new();
+        bf2.insert(3);
+        bf2.insert(4); // 000110 000000
+        let mut bf3 = RoaringBitmap::new();
+        bf3.insert(5); // 000 001
+
+        //expected
+        // 011000000110000 000000000000001 [1,2,9,10,29]
+
+        let chunk_files = vec![
+            chunk1_path.clone(),
+            chunk2_path.clone(),
+            chunk3_path.clone(),
+        ];
+        let color_counts = vec![2, 2, 1]; //nb of colors for each chunk
+
+        for (chunk_path, (bf, colors)) in chunk_files.iter().zip(vec![(bf1, 2), (bf2, 2), (bf3, 1)])
+        {
+            let mut file = File::create(chunk_path).expect("Failed to create test chunk file");
+            file.write_all(&(colors as u64).to_le_bytes())
+                .expect("Failed to write color count");
+            bf.serialize_into(&mut file)
+                .expect("Failed to serialize Bloom filter");
+        }
+
+        let path = Path::new(test_dir).join("partition_bloom_filters.bin");
+        let file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create_new(true)
+            .open(&path)
+            .unwrap();
+        let mut writer = BufWriter::new(file);
+        let len = 1;
+        tar_get::reserve_capacity(&mut writer, len).unwrap(); // TODO expect
+
+        let mut file = writer.into_inner().unwrap();
+
+        // Call the modified function
+        let merged_bf = merge_partition_bloom_filters(
+            &chunk_files,
+            partitioned_bf_size,
+            abundance_number,
+            &color_counts,
+            &mut file,
+        )
+        .expect("Failed to merge partition Bloom filters");
+
+        // check the merged Bloom filter
+        let expected_elements: Vec<u32> = vec![1, 2, 9, 10, 29];
+        for elem in expected_elements {
+            assert!(
+                merged_bf.contains(elem),
+                "Expected element {} not found in the merged Bloom filter",
+                elem
+            );
+        }
+    }
+}
---
+++ src/reindeer2/merge/mod.rs
@@ -0,0 +1,70 @@
+mod merge_indexes;
+mod merge_partitions;
+
+use roaring::RoaringBitmap;
+
+pub use merge_indexes::merge_multiple_indexes;
+pub use merge_partitions::merge_all_partitions_of_chunks;
+
+/* example for build_new_bitset_with_gaps_from_merged + interleave_slices_with_zero_runs
+ merged = 110001 000000 (2 "colums" in the partition, 2 datasets, 3 abundances)
+ bf to add = 111 000 (2 "colums" in the partition, 1 dataset, 3 abundances)
+
+ build_new_bitset_with_gaps_from_merge will prepare merge as follows:
+
+ 110001 000 000000 000 <- new runs of 0 of size 3 to add abundances for the new dataset brought by bf_to_add
+
+ conversely, interleave_slices_with_zero_runs will prepare bf_to_add for the union:
+
+ 000000 111 000000 000 <- new runs of 0 of size 2*3 (size of a block in the merged vector)
+
+ then we perform the union
+
+ 110001111 000000000 -> 2 "colums" in the partition, THREE datasets, 3 abundances
+
+*/
+
+/// Merges all filter from files `chunk_files` into a `RoaringBitmap`.
+fn merge_partition_slices_interleaved(
+    filters: &[(RoaringBitmap, usize)],
+    partitioned_bf_size: usize,
+    abundance_number: usize,
+    // color_counts: &[usize], // number of colors in each chunk TODO enlever
+) -> RoaringBitmap {
+    // preload all Bfs
+
+    // vector to collect all positions for the final bf
+    let mut final_positions = Vec::new();
+
+    let sum_color: usize = filters.iter().map(|(_filter, nb_color)| nb_color).sum();
+
+    for slice_idx in 0..partitioned_bf_size {
+        let mut current_offset = slice_idx * abundance_number * sum_color;
+
+        for (filter, nb_color) in filters {
+            let slice_start = slice_idx * abundance_number * nb_color;
+            let slice_end = slice_start + abundance_number * nb_color;
+
+            let slice_start_u32 = slice_start as u32;
+            let current_offset_u32 = current_offset as u32;
+            let slice_end_u32 = slice_end as u32;
+            #[cfg(any(debug_assertions, test))]
+            {
+                assert_eq!(slice_start, slice_start_u32 as usize);
+                assert_eq!(current_offset, current_offset_u32 as usize);
+                assert_eq!(slice_end, slice_end_u32 as usize);
+            }
+            // collect positions in the slice and adjust by offset
+            final_positions.extend(
+                filter
+                    .range(slice_start_u32..slice_end_u32)
+                    .map(|pos| pos - slice_start_u32 + current_offset_u32),
+            );
+
+            // update the offset for the next chunk in this slice
+            current_offset += abundance_number * nb_color;
+        }
+    }
+    RoaringBitmap::from_sorted_iter(final_positions)
+        .expect("Attempt to merge with unsorted positions")
+}
